0be4d1f4ddf57a8d656a3feb715ad0a33373ef88
Oluwatuyi Timilehin Ezekiel <timilehinoluwatuyi98@gmail.com>
2023-08-04 09:39:18 +0100

    First Commit

---
+++ learn.py
@@ -0,0 +1,16 @@
+name = input("Name: ")
+if name:
+    print(f"The name you input is {name}")
+
+class print():
+    def __init__(self, x, y)
+        self.x = x
+        self.y = x
+    
+    def printValues(self)
+        print(f"{this.x} and {this.y}")
+
+
+timmy = print(9, 10)
+
+timmy.printValues()
