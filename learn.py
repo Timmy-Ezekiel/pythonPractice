86b9e927d685a3321c33ba8c77ea5e5d87224faa
Oluwatuyi Timilehin Ezekiel <timilehinoluwatuyi98@gmail.com>
2023-08-04 10:47:16 +0100

    Run the code perfectly

--- learn.py
+++ learn.py
@@ -2,25 +2,18 @@ name = input("Name: ")
 if name:
     print(f"The name you input is {name}")
 
-class print():
-    def __init__(self, x, y)
+class produce():
+    def __init__(self, x, y):
         self.x = x
-        self.y = x
+        self.y = y
     
-    def printValues(self)
-        print(f"{this.x} and {this.y}")
-        self.run
+    def printValues(self):
+        print(f"{self.x} and {self.y}")
+        self.run()
 
-    def run(self)
+    def run(self):
         print("Welcome")
 
-timmy = print(9, 10)
+timmy = produce(9, 10)
 
 timmy.printValues()
-
-states = ["Delta", "Ogun", "Edo", "Lagos", "Anambra"]
-for name in states
-    print(f"{name}")
-for i in range(50)
-    if i%2 == 0
-        print(i)
